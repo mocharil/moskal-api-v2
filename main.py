0f91225bac04ce18d486dcbc10172fcabd1d1e41
Moch. Aril Indra Permana <46191697+mocharil@users.noreply.github.com>
2025-04-20 23:42:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -259,9 +259,3 @@ if __name__ == "__main__":
     port = int(os.getenv("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
-
-# YANG BELUM
-"""
-- Topics dan KOL:
-    Jika belum tergenerate harus gimana?
-"""
